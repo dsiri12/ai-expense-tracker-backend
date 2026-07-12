95512018b8f0689550744eeeb99f57a93a1b0720
Damien Siri <dsiri12@gmail.com>
2026-07-11 21:08:45 -0400

    add login validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,13 +13,6 @@ const signToken = (userId) =>
 export const register = async (req, res) => {
     const { name, email, password, currency = 'USD' } = req.body;
 
-    // if (!name || !email || !password) {
-    //     return res.status(400).json({ message: 'Name, email, and password are required' });
-    // }
-    // if (password.length < 6) {
-    //     return res.status(400).json({ message: 'Password must be at least 6 characters' });
-    // }
-
     const client = await pool.connect(); // Needed for transactions.
     try {
         const existing = await client.query('SELECT id FROM users WHERE email = $1', [email]);
@@ -71,10 +64,6 @@ export const register = async (req, res) => {
  */
 export const login = async (req, res) => {
     const { email, password } = req.body;
-
-    if (!email || !password) {
-        return res.status(400).json({ message: 'Email and password are required' });
-    }
 
     try {
         const result = await pool.query(
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,12 @@ import express from 'express';
 import { register, login, getMe } from '../controllers/authController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { validate } from "../middleware/validate.js";
-import { registerSchema } from "../validators/auth.validator.js";
+import { registerSchema, loginSchema } from "../validators/auth.validator.js";
 
 const router = express.Router();
 
 router.post('/register', validate(registerSchema), register);
-router.post('/login', login);
+router.post('/login', validate(loginSchema), login);
 router.get('/me', protect, getMe);
 
 export default router;
